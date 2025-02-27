84d5b8e64bf252f8a309c26d94a1bb9f7e334910
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2025-02-27 11:54:49 +0000

    Create simple_app.py

---
+++ simple_app.py
@@ -0,0 +1,47 @@
+import gradio as gr
+import subprocess
+from huggingface_hub import snapshot_download
+
+#Download model
+snapshot_download(
+    repo_id = "Wan-AI/Wan2.1-T2V-1.3B",
+    local_dir = "./Wan2.1-T2V-1.3B"
+)
+
+def infer(prompt):
+    prompt = ""Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage.""
+    
+    command = [
+        "python", "-m", "generate.py",
+        "--task", "t2v-1.3B",
+        "--size", "832*480",
+        "--ckpt_dir", "./Wan2.1-T2V-1.3B",
+        "--sample_shift", "8",
+        "--sample_guide_scale", "6",
+        "--prompt", f"{prompt}",
+        "--save_file", "generated_video.mp4"
+    ]
+
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    if result.returncode == 0:
+        print("Command executed successfully.")
+        return "./tmp/"generated_video.mp4""
+    else:
+        print("Error executing command.")
+        raise gr.Error("Error executing command")
+
+with gr.Blocks() as demo:
+    with gr.Column():
+        gr.Markdown("# Wan 2.1")
+        prompt = gr.Textbox(labe="Prompt")
+        submit_btn = gr.Button("Submit")
+        video_res = gr.Video(label="Generated Video")
+
+    submit_btn.click(
+        fn = infer,
+        inputs = [prompt],
+        outputs = [video_res]
+    )
+
+demo.queue().launch(show_error=True, show_api=False, ssr_mode=False)
